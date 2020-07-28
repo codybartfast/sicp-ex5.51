d7c0cc1e98e9429a00d638cdab99927245bd0809
John Donnellan <john@codybartfast.com>
2020-07-28 09:17:38 +0100

    rename to sopen +

--- src/sicp.c
+++ src/sicp.c
@@ -8,19 +8,19 @@ int main(void)
 	source *source;
 
 	printf("\nreading source from NAMED file:\n    ");
-	source = source_name("file.txt");
+	source = sopen_name("file.txt");
 	parse(source);
-	source_close(source);
+	sclose(source);
 
 	printf("\nreading source from a STRING:\n    ");
-	source = source_string("I am a string");
+	source = sopen_string("(i am a string)");
 	parse(source);
-	source_close(source);
+	sclose(source);
 
 	printf("\n\nreading source from FILE pointer:\n    ");
-	source = source_file(stdin);
+	source = sopen_file(stdin);
 	parse(source);
-	source_close(source);
+	sclose(source);
 	printf("\n");
 	
 	return 0;
