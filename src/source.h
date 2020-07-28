c15c97109be132c22ecd786a5dcac672aa9c3971
John Donnellan <john@codybartfast.com>
2020-07-28 10:22:20 +0100

    add comments

--- src/source.h
+++ src/source.h
@@ -1,19 +1,24 @@
+/*
+ * Why?  Expect to be reading source from a file, but for other use case, e.g.,
+ * testing and dev, it will be useful to parse a string directly.  sgetc(source)
+ * is intended to be used in the same way as getc(file).
+ *
+ * Could use fmemopen, but it's not portable.
+ *
+ * The block struct could use a union, but I'm not sure that gives anything
+ * other than saving a few bytes.
+ */
+
 #include <stdio.h>
 
-enum source_kind
-{
-    SOURCE_FILE_POINTER,
-    SOURCE_FILE_NAME,
-    SOURCE_STRING
-};
+enum source_kind { SOURCE_FILE_POINTER, SOURCE_FILE_NAME, SOURCE_STRING };
 
-typedef struct
-{
-    enum source_kind kind;
-    char *name;
-    FILE *file;
-    char *text;
-    char *next;
+typedef struct {
+	enum source_kind kind;
+	char *name;
+	FILE *file;
+	char *text;
+	char *next;
 } source;
 
 source *sopen_file(FILE *);
