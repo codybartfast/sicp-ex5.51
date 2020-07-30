a13f6219e182d7da89c9916ffb4ad9027d033bc3
John Donnellan <john@codybartfast.com>
2020-07-30 07:47:22 +0100

    rename sopen_ to src_ +

--- src/source.h
+++ src/source.h
@@ -11,20 +11,20 @@
 
 #include <stdio.h>
 
-enum source_kind { SOURCE_FILE_POINTER, SOURCE_FILE_NAME, SOURCE_STRING };
+enum src_kind { SRC_FILE_POINTER, SRC_FILE_NAME, SRC_STRING };
 
-typedef struct {
-	enum source_kind kind;
+typedef struct source {
+	enum src_kind kind;
 	char *name;
 	FILE *file;
 	char *text;
 	char *next;
 } source;
 
-source *sopen_file(FILE *);
-source *sopen_name(char *);
-source *sopen_string(char *);
+source *src_file(FILE *);
+source *src_name(char *);
+source *src_string(char *);
 
-int sclose(source *);
+int src_close(source *);
 
 int sgetc(source *);
