f7f20e98eac5e11cd69af19f1b474a8d66277e3e
John Donnellan <john@codybartfast.com>
2020-08-30 11:12:46 +0100

    Wip: add storage.c

---
+++ src/storage.c
@@ -0,0 +1,8 @@
+#include "storage.h"
+
+#include <stdlib.h>
+
+struct cell *newcell(bool nogc){
+	(void)nogc;
+	return NULL;
+}
---
+++ src/storage.h
@@ -0,0 +1,10 @@
+#ifndef STORAGE_H
+#define STORAGE_H
+#include "sicpstd.h"
+
+#include <stdbool.h>
+#include "obj.h"
+
+struct cell *newcell(bool nogc);
+
+#endif
