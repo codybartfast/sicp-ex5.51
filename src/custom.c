0a1bf474cdc9fe3b79de0c7dfa2bf87da7bb9eff
John Donnellan <john@codybartfast.com>
2020-09-27 09:06:27 +0100

    Add: filter, accumulate

--- src/custom.c
+++ src/custom.c
@@ -483,6 +483,19 @@ static obj add_extras(int ex, obj env)
 			"                    mapped))))"
 			"  (reverse (iter arglists nil)))",
 			env);
+		evalstr("(define (filter predicate sequence)"
+			"  (cond ((null? sequence) nil)"
+			"        ((predicate (car sequence))"
+			"         (cons (car sequence)"
+			"               (filter predicate (cdr sequence))))"
+			"        (else (filter predicate (cdr sequence)))))",
+			env);
+		evalstr("(define (accumulate op initial sequence)"
+			"  (if (null? sequence)"
+			"      initial"
+			"      (op (car sequence)"
+			"          (accumulate op initial (cdr sequence)))))",
+			env);
 	}
 	if (ex >= 224) {
 		define_variable(of_identifier("pair?"), of_function(is_pair_p),
