d43a93bb3c0a4f223eff662c07bbff6d3a7c24e6
John Donnellan <john@codybartfast.com>
2020-10-20 18:38:30 +0100

    Add: core

--- src/eceval.c
+++ src/eceval.c
@@ -13,18 +13,23 @@
 
 #define AREA "ECEVAL"
 
-static obj argl; // 1
-static obj cont; // 2
-static obj env; // 3
-static obj expr; // 4
-static obj proc; // 5
-static obj stack; // 6
-static obj unev; // 7
-static obj val; // 8
-// Plus... the_global_environment // 9
-obj anenv; // 10
-obj ambenv; // 11
+struct core {
+	obj argl; //     1
+	obj cont; //     2
+	obj env; //      3
+	obj expr; //     4
+	obj proc; //     5
+	obj stack; //    6
+	obj unev; //     7
+	obj val; //      8
+} cre;
+struct core *cr0 = &cre;
+
+// the_global_environment // 9
+obj anenv; //   10
+obj ambenv; //  11
 obj savetmp; // 12
+
 const int rootlen = 12;
 static obj rootlst;
 
@@ -57,26 +62,26 @@ static void timed_eval(obj start)
 	displaydat(of_string("s ] "));
 }
 
-static void save(obj val)
+static void save(obj dat, struct core *cr)
 {
-	savetmp = val;
-	stack = consgc(&savetmp, &stack);
+	savetmp = dat;
+	cr->stack = consgc(&savetmp, &cr->stack);
 
-	if (is_err(stack)) {
+	if (is_err(cr->stack)) {
 		eprintf(AREA, "Halting - Reached Memory Limit");
 		exit(1);
 	}
 	return;
 }
 
-static obj restore(void)
+static obj restore(struct core *cr)
 {
-	if (is_null(stack)) {
-		eprintf(AREA, "attempted to restore from empty stack");
+	if (is_null(cr->stack)) {
+		eprintf(AREA, "attempted to restore from empty cr->stack");
 		exit(1);
 	} else {
-		obj dat = car(stack);
-		stack = cdr(stack);
+		obj dat = car(cr->stack);
+		cr->stack = cdr(cr->stack);
 		return dat;
 	}
 }
@@ -97,21 +102,21 @@ obj getroot(void)
 		exit(1);
 	}
 	// order must match setroot
-	set_car(lst, argl);
+	set_car(lst, cr0->argl);
 	lst = cdr(lst);
-	set_car(lst, cont);
+	set_car(lst, cr0->cont);
 	lst = cdr(lst);
-	set_car(lst, env);
+	set_car(lst, cr0->env);
 	lst = cdr(lst);
-	set_car(lst, expr);
+	set_car(lst, cr0->expr);
 	lst = cdr(lst);
-	set_car(lst, proc);
+	set_car(lst, cr0->proc);
 	lst = cdr(lst);
-	set_car(lst, stack);
+	set_car(lst, cr0->stack);
 	lst = cdr(lst);
-	set_car(lst, unev);
+	set_car(lst, cr0->unev);
 	lst = cdr(lst);
-	set_car(lst, val);
+	set_car(lst, cr0->val);
 	lst = cdr(lst);
 	set_car(lst, the_global_environment());
 	lst = cdr(lst);
@@ -139,21 +144,21 @@ obj setroot(obj rlst)
 			actlen);
 	}
 	// order must match getroot
-	argl = car(lst);
+	cr0->argl = car(lst);
 	lst = cdr(lst);
-	cont = car(lst);
+	cr0->cont = car(lst);
 	lst = cdr(lst);
-	env = car(lst);
+	cr0->env = car(lst);
 	lst = cdr(lst);
-	expr = car(lst);
+	cr0->expr = car(lst);
 	lst = cdr(lst);
-	proc = car(lst);
+	cr0->proc = car(lst);
 	lst = cdr(lst);
-	stack = car(lst);
+	cr0->stack = car(lst);
 	lst = cdr(lst);
-	unev = car(lst);
+	cr0->unev = car(lst);
 	lst = cdr(lst);
-	val = car(lst);
+	cr0->val = car(lst);
 	lst = cdr(lst);
 	set_global_environment(car(lst));
 	lst = cdr(lst);
@@ -167,13 +172,13 @@ obj setroot(obj rlst)
 }
 
 static obj proc_name;
-static void set_proc_name(void)
+static void set_proc_name(struct core *cr)
 {
-	proc_name = is_symbol(expr) ?
-			    expr :
-			    (is_pair(expr) && is_eq(car(expr), lambda)) ?
-			    lambda :
-			    of_string("<unknown>");
+	proc_name = is_symbol(cr->expr) ? cr->expr :
+					  (is_pair(cr->expr) &&
+					   is_eq(car(cr->expr), lambda)) ?
+					  lambda :
+					  of_string("<unknown>");
 }
 
 static bool initdone = false;
@@ -181,7 +186,7 @@ static obj init(void)
 {
 	int actlen;
 
-	stack = emptylst;
+	cr0->stack = emptylst;
 	// preallocate storage for gc root
 	rootlst = listn(12, //       <----- actual  length
 			unspecified, //  1
@@ -208,64 +213,64 @@ static obj init(void)
 	return unspecified;
 }
 
-obj eceval(obj expression, obj _environment)
+static obj ecevalcr(obj expression, obj _environment, struct core *cr)
 {
 	if (!initdone) {
 		init();
 	}
-	expr = expression;
-	env = _environment;
-	cont = ev_return_caller;
+	cr->expr = expression;
+	cr->env = _environment;
+	cr->cont = ev_return_caller;
 
 // 5.4.1 The Core of the Evaluator
 
 // ln 236
 eval_dispatch:
-	if (is_err(expr))
-		return expr;
+	if (is_err(cr->expr))
+		return cr->expr;
 
 	// printf("eval: ");
-	// printf("%s\n", errstr(expr));
+	// printf("%s\n", errstr(cr->expr));
 
-	if (is_self_evaluating(expr))
+	if (is_self_evaluating(cr->expr))
 		goto ev_self_eval;
-	if (is_variable(expr))
+	if (is_variable(cr->expr))
 		goto ev_variable;
-	if (is_quoted(expr))
+	if (is_quoted(cr->expr))
 		goto ev_quoted;
-	if (is_quasiquote(expr))
+	if (is_quasiquote(cr->expr))
 		goto ev_quasiquoted;
-	if (is_assignment(expr))
+	if (is_assignment(cr->expr))
 		goto ev_assignment;
-	if (is_definition(expr))
+	if (is_definition(cr->expr))
 		goto ev_definition;
-	if (is_if(expr))
+	if (is_if(cr->expr))
 		goto ev_if;
-	if (is_lambda(expr))
+	if (is_lambda(cr->expr))
 		goto ev_lambda;
-	if (is_begin(expr))
+	if (is_begin(cr->expr))
 		goto ev_begin;
-	if (is_cond(expr))
+	if (is_cond(cr->expr))
 		goto ev_cond;
-	if (is_let(expr))
+	if (is_let(cr->expr))
 		goto ev_let;
-	if (is_letrec(expr))
+	if (is_letrec(cr->expr))
 		goto ev_letrec;
-	if (is_letstar(expr))
+	if (is_letstar(cr->expr))
 		goto ev_letstar;
-	if (is_and(expr))
+	if (is_and(cr->expr))
 		goto ev_and;
-	if (is_or(expr))
+	if (is_or(cr->expr))
 		goto ev_or;
-	if (is_delay(expr))
+	if (is_delay(cr->expr))
 		goto ev_delay;
-	if (is_cons_stream(expr))
+	if (is_cons_stream(cr->expr))
 		goto ev_cons_stream;
-	if (is_time(expr))
+	if (is_time(cr->expr))
 		goto ev_timed;
-	if (is_ecapply(expr))
+	if (is_ecapply(cr->expr))
 		goto ev_apply;
-	if (is_application(expr))
+	if (is_application(cr->expr))
 		goto ev_application;
 	goto unknown_expression_type;
 
@@ -273,308 +278,315 @@ eval_dispatch:
 
 // ln 259
 ev_self_eval:
-	val = expr;
+	cr->val = cr->expr;
 	goto go_cont;
 ev_variable:
-	val = lookup_variable_value(expr, env);
-	if (is_err(val))
-		return val;
+	cr->val = lookup_variable_value(cr->expr, cr->env);
+	if (is_err(cr->val))
+		return cr->val;
 	goto go_cont;
 ev_quoted:
-	val = text_of_quotation(expr);
+	cr->val = text_of_quotation(cr->expr);
 	goto go_cont;
 ev_quasiquoted:
-	expr = quasi_to_combination(expr);
+	cr->expr = quasi_to_combination(cr->expr);
 	goto eval_dispatch;
 ev_lambda:
-	unev = lambda_parameters(expr);
-	expr = lambda_body(expr);
-	val = make_procedure(unev, expr, env);
+	cr->unev = lambda_parameters(cr->expr);
+	cr->expr = lambda_body(cr->expr);
+	cr->val = make_procedure(cr->unev, cr->expr, cr->env);
 	goto go_cont;
 
 // Evaluating procedure applications
 
 // ln 277
 ev_application:
-	save(cont);
-	save(env);
-	unev = operands(expr);
-	save(unev);
-	expr = operator(expr);
-	set_proc_name();
-	cont = ev_appl_did_operator;
+	save(cr->cont, cr);
+	save(cr->env, cr);
+	cr->unev = operands(cr->expr);
+	save(cr->unev, cr);
+	cr->expr = operator(cr->expr);
+	set_proc_name(cr);
+	cr->cont = ev_appl_did_operator;
 	goto eval_dispatch;
 
 // ln 286
 ev_appl_did_operator:
-	unev = restore(); // the operands
-	env = restore();
-	argl = empty_arglist();
-	proc = val; // the operator
-	if (is_err(proc))
-		return proc;
-	if (no_operands(unev))
+	cr->unev = restore(cr); // the operands
+	cr->env = restore(cr);
+	cr->argl = empty_arglist();
+	cr->proc = cr->val; // the operator
+	if (is_err(cr->proc))
+		return cr->proc;
+	if (no_operands(cr->unev))
 		goto apply_dispatch;
-	save(proc);
+	save(cr->proc, cr);
 
 // ln 295
 ev_appl_operand_loop:
-	save(argl);
-	expr = first_operand(unev);
-	if (is_last_operand(unev))
+	save(cr->argl, cr);
+	cr->expr = first_operand(cr->unev);
+	if (is_last_operand(cr->unev))
 		goto ev_appl_last_arg;
-	save(env);
-	save(unev);
-	cont = ev_appl_accumulate_arg;
+	save(cr->env, cr);
+	save(cr->unev, cr);
+	cr->cont = ev_appl_accumulate_arg;
 	goto eval_dispatch;
 
 // ln 305
 ev_appl_accumulate_arg:
-	unev = restore();
-	env = restore();
-	argl = restore();
-	argl = adjoin_arg(val, argl);
-	unev = rest_operands(unev);
+	cr->unev = restore(cr);
+	cr->env = restore(cr);
+	cr->argl = restore(cr);
+	cr->argl = adjoin_arg(cr->val, cr->argl);
+	cr->unev = rest_operands(cr->unev);
 	goto ev_appl_operand_loop;
 
 // ln 313
 ev_appl_last_arg:
-	cont = ev_appl_accum_last_arg;
+	cr->cont = ev_appl_accum_last_arg;
 	goto eval_dispatch;
 ev_appl_accum_last_arg:
-	argl = restore();
-	argl = adjoin_arg(val, argl);
-	proc = restore();
+	cr->argl = restore(cr);
+	cr->argl = adjoin_arg(cr->val, cr->argl);
+	cr->proc = restore(cr);
 	goto apply_dispatch;
 
 // Procedure application
 
 // ln 324
 apply_dispatch:
-	if (is_primitive_procedure(proc))
+	if (is_primitive_procedure(cr->proc))
 		goto primitive_apply;
-	if (is_compound_procedure(proc))
+	if (is_compound_procedure(cr->proc))
 		goto compound_apply;
 	goto unknown_procedure_type;
 
 // ln 331
 primitive_apply:
-	val = apply_primitive_procedure(proc, argl);
-	if (is_err(val))
-		return val;
-	cont = restore();
+	cr->val = apply_primitive_procedure(cr->proc, cr->argl);
+	if (is_err(cr->val))
+		return cr->val;
+	cr->cont = restore(cr);
 	goto go_cont;
 
 // ln 348
 ev_begin:
-	unev = begin_actions(expr);
-	save(cont);
+	cr->unev = begin_actions(cr->expr);
+	save(cr->cont, cr);
 	goto ev_sequence;
 
 // ln 338
 compound_apply:
-	unev = procedure_parameters(proc);
-	env = procedure_environment(proc);
-	env = extend_environment(unev, argl, env, proc_name);
-	if (is_err(env))
-		return env;
-	unev = procedure_body(proc);
+	cr->unev = procedure_parameters(cr->proc);
+	cr->env = procedure_environment(cr->proc);
+	cr->env = extend_environment(cr->unev, cr->argl, cr->env, proc_name);
+	if (is_err(cr->env))
+		return cr->env;
+	cr->unev = procedure_body(cr->proc);
 	goto ev_sequence;
 
 // 5.4.2 Sequence Evaluation and Tail Recursion
 
 // ln 353
 ev_sequence:
-	expr = first_exp(unev);
-	if (is_last_exp(unev))
+	cr->expr = first_exp(cr->unev);
+	if (is_last_exp(cr->unev))
 		goto ev_sequence_last_exp;
-	save(unev);
-	save(env);
-	cont = ev_sequence_continue;
+	save(cr->unev, cr);
+	save(cr->env, cr);
+	cr->cont = ev_sequence_continue;
 	goto eval_dispatch;
 ev_sequence_continue:
-	env = restore();
-	unev = restore();
-	unev = rest_exps(unev);
+	cr->env = restore(cr);
+	cr->unev = restore(cr);
+	cr->unev = rest_exps(cr->unev);
 	goto ev_sequence;
 ev_sequence_last_exp:
-	cont = restore();
+	cr->cont = restore(cr);
 	goto eval_dispatch;
 
 // 5.4.3 Conditionals, Assignments and Definitions
 
 // ln 374
 ev_if:
-	save(expr); // save expression for later
-	save(env);
-	save(cont);
-	cont = ev_if_decide;
-	expr = if_predicate(expr);
+	save(cr->expr, cr); // save expression for later
+	save(cr->env, cr);
+	save(cr->cont, cr);
+	cr->cont = ev_if_decide;
+	cr->expr = if_predicate(cr->expr);
 	goto eval_dispatch; // evaluate the predicate
 
 // ln 382
 ev_if_decide:
-	cont = restore();
-	env = restore();
-	expr = restore();
-	if (is_true(val))
+	cr->cont = restore(cr);
+	cr->env = restore(cr);
+	cr->expr = restore(cr);
+	if (is_true(cr->val))
 		goto ev_if_consequent;
 
 	// ln 389
 	//ev_if_alternative:
-	expr = if_alternate(expr);
+	cr->expr = if_alternate(cr->expr);
 	goto eval_dispatch;
 ev_if_consequent:
-	expr = if_consequent(expr);
+	cr->expr = if_consequent(cr->expr);
 	goto eval_dispatch;
 
 // ln 399
 ev_assignment:
-	unev = assignment_variable(expr);
-	save(unev); // save variable for later
-	expr = assignment_value(expr);
-	save(env);
-	save(cont);
-	cont = ev_assignment_1;
+	cr->unev = assignment_variable(cr->expr);
+	save(cr->unev, cr); // save variable for later
+	cr->expr = assignment_value(cr->expr);
+	save(cr->env, cr);
+	save(cr->cont, cr);
+	cr->cont = ev_assignment_1;
 	goto eval_dispatch; // evaluate the assignment value
 
 ev_assignment_1:
-	cont = restore();
-	env = restore();
-	unev = restore();
-	set_variable_value(unev, val, env);
-	val = ok;
+	cr->cont = restore(cr);
+	cr->env = restore(cr);
+	cr->unev = restore(cr);
+	set_variable_value(cr->unev, cr->val, cr->env);
+	cr->val = ok;
 	goto go_cont;
 
 // ln 416
 ev_definition:
-	unev = definition_variable(expr);
-	save(unev); // save variable for later
-	expr = definition_value(expr);
-	save(env);
-	save(cont);
-	cont = ev_definition_1;
+	cr->unev = definition_variable(cr->expr);
+	save(cr->unev, cr); // save variable for later
+	cr->expr = definition_value(cr->expr);
+	save(cr->env, cr);
+	save(cr->cont, cr);
+	cr->cont = ev_definition_1;
 	goto eval_dispatch; // evaluate the definition value
 ev_definition_1:
-	cont = restore();
-	env = restore();
-	unev = restore();
-	define_variable(unev, val, env);
-	val = ok;
+	cr->cont = restore(cr);
+	cr->env = restore(cr);
+	cr->unev = restore(cr);
+	define_variable(cr->unev, cr->val, cr->env);
+	cr->val = ok;
 	goto go_cont;
 
 ev_cond:
-	expr = cond_to_if(expr);
+	cr->expr = cond_to_if(cr->expr);
 	goto eval_dispatch;
 
 // new - let
 ev_let:
-	expr = let_to_combination(expr);
+	cr->expr = let_to_combination(cr->expr);
 	goto ev_application;
 
 // new - letrec
 ev_letrec:
-	expr = letrec_to_combination(expr);
+	cr->expr = letrec_to_combination(cr->expr);
 	goto ev_application;
 
 // new - letstar
 ev_letstar:
-	expr = letstar_to_combination(expr);
+	cr->expr = letstar_to_combination(cr->expr);
 	goto ev_application;
 
 // new - and
 ev_and:
-	expr = and_to_if(expr);
+	cr->expr = and_to_if(cr->expr);
 	goto eval_dispatch;
 
 // new - or
 ev_or:
-	expr = or_to_if(expr);
+	cr->expr = or_to_if(cr->expr);
 	goto eval_dispatch;
 
 // new - delay / cons-stream
 ev_delay:
-	expr = delay_to_lambda(expr);
+	cr->expr = delay_to_lambda(cr->expr);
 	goto ev_lambda;
 
 ev_cons_stream:
-	expr = cons_stream_to_cons(expr);
+	cr->expr = cons_stream_to_cons(cr->expr);
 	goto ev_application;
 
 // new
 ev_timed:
-	save(unev);
-	unev = runtime(emptylst);
-	save(unev);
-	save(cont);
-	cont = ev_timed_done;
-	expr = cons(begin, cdr(expr));
+	save(cr->unev, cr);
+	cr->unev = runtime(emptylst);
+	save(cr->unev, cr);
+	save(cr->cont, cr);
+	cr->cont = ev_timed_done;
+	cr->expr = cons(begin, cdr(cr->expr));
 	goto eval_dispatch;
 
 ev_timed_done:
-	cont = restore();
-	unev = restore();
-	timed_eval(unev);
-	unev = restore();
+	cr->cont = restore(cr);
+	cr->unev = restore(cr);
+	timed_eval(cr->unev);
+	cr->unev = restore(cr);
 	goto go_cont;
 
 // new
 ev_apply:
-	save(cont);
-	save(env);
-	unev = apply_operands(expr);
-	save(unev);
-	expr = apply_operator(expr);
-	cont = ev_apply_2;
+	save(cr->cont, cr);
+	save(cr->env, cr);
+	cr->unev = apply_operands(cr->expr);
+	save(cr->unev, cr);
+	cr->expr = apply_operator(cr->expr);
+	cr->cont = ev_apply_2;
 	goto eval_dispatch;
 
 ev_apply_2:
-	unev = restore();
-	env = restore();
-	proc = val;
-	save(proc);
-	save(env);
-	expr = unev;
-	cont = ev_apply_3;
+	cr->unev = restore(cr);
+	cr->env = restore(cr);
+	cr->proc = cr->val;
+	save(cr->proc, cr);
+	save(cr->env, cr);
+	cr->expr = cr->unev;
+	cr->cont = ev_apply_3;
 	goto eval_dispatch;
 
 ev_apply_3:
-	env = restore();
-	proc = restore();
-	argl = val;
+	cr->env = restore(cr);
+	cr->proc = restore(cr);
+	cr->argl = cr->val;
 	goto apply_dispatch;
 
 // ln 433
 unknown_expression_type:
-	return error_eval(AREA, "Unknown expression type: %s", errstr(expr));
+	return error_eval(AREA, "Unknown expression type: %s",
+			  errstr(cr->expr));
 unknown_procedure_type:
-	cont = restore(); // clean up stack (from apply-dispatch)
-	return error_eval(AREA, "Unknown procedure type: %s", errstr(proc));
+	cr->cont = restore(cr); // clean up cr->stack (from apply-dispatch)
+	return error_eval(AREA, "Unknown procedure type: %s", errstr(cr->proc));
 
 go_cont:
-	if (is_eq(cont, ev_return_caller))
-		return val;
-	if (is_eq(cont, ev_appl_accum_last_arg))
+	if (is_eq(cr->cont, ev_return_caller))
+		return cr->val;
+	if (is_eq(cr->cont, ev_appl_accum_last_arg))
 		goto ev_appl_accum_last_arg;
-	if (is_eq(cont, ev_appl_accumulate_arg))
+	if (is_eq(cr->cont, ev_appl_accumulate_arg))
 		goto ev_appl_accumulate_arg;
-	if (is_eq(cont, ev_appl_did_operator))
+	if (is_eq(cr->cont, ev_appl_did_operator))
 		goto ev_appl_did_operator;
-	if (is_eq(cont, ev_apply_2))
+	if (is_eq(cr->cont, ev_apply_2))
 		goto ev_apply_2;
-	if (is_eq(cont, ev_apply_3))
+	if (is_eq(cr->cont, ev_apply_3))
 		goto ev_apply_3;
-	if (is_eq(cont, ev_assignment_1))
+	if (is_eq(cr->cont, ev_assignment_1))
 		goto ev_assignment_1;
-	if (is_eq(cont, ev_definition_1))
+	if (is_eq(cr->cont, ev_definition_1))
 		goto ev_definition_1;
-	if (is_eq(cont, ev_if_decide))
+	if (is_eq(cr->cont, ev_if_decide))
 		goto ev_if_decide;
-	if (is_eq(cont, ev_quoted))
+	if (is_eq(cr->cont, ev_quoted))
 		goto ev_quoted;
-	if (is_eq(cont, ev_sequence_continue))
+	if (is_eq(cr->cont, ev_sequence_continue))
 		goto ev_sequence_continue;
-	if (is_eq(cont, ev_timed_done))
+	if (is_eq(cr->cont, ev_timed_done))
 		goto ev_timed_done;
-	return error_internal(AREA, "BUG! Goto... where?: %s", to_string(cont));
+	return error_internal(AREA, "BUG! Goto... where?: %s",
+			      to_string(cr->cont));
+}
+
+obj eceval(obj expression, obj _environment)
+{
+	return ecevalcr(expression, _environment,cr0);
 }
